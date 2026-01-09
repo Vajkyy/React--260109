9ddcb05eb7716fb1ea1e5417b9baf93513605707
Vajkyy <vajki2014@gmail.com>
2026-01-09 10:39:41 +0100

    add error message

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,31 +1,38 @@
-import React from 'react'
-import {NavLink} from "react-router "
+import React from "react";
+import { NavLink } from "react-router ";
 
 export default function LoginPage() {
   return (
-    <div className='login'>
-        <h1>WELCOME BACK</h1>
-        <form onClick={submit}>
-            <div>
-                <label htmlFor="email">EMAIL ADDRESS</label>
-                <input type="email"
-                value={email}
-                placeholder='Enter your email' 
-                id='email'/>                
-            </div>
-            <div>
-                <label htmlFor="password">PASSWORD</label>
-                <input type="password"
-                value={password}
-                placeholder='Enter your password'
-                id='password' />
-            </div>
-            <div className='descript'>
-                Registration is free!{" "}
-                <NavLink to="/register">CREATE AN ACCOUNT</NavLink>
-            </div>
-
-        </form>
+    <div className="login">
+      <h1>WELCOME BACK</h1>
+      <form onClick={submit}>
+        <div>
+          <label htmlFor="email">EMAIL ADDRESS</label>
+          <input
+            type="email"
+            value={email}
+            placeholder="Enter your email"
+            id="email"
+          />
+          {error.email && <span className="error-text">errors.email</span>}
+        </div>
+        <div>
+          <label htmlFor="password">PASSWORD</label>
+          <input
+            type="password"
+            value={password}
+            placeholder="Enter your password"
+            id="password"
+          />
+          {errors.password && (
+            <span className="error-text">{errors.password}</span>
+          )}
+        </div>
+        <div className="descript">
+          Registration is free!{" "}
+          <NavLink to="/register">CREATE AN ACCOUNT</NavLink>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
--- frontend/src/pages/RegistrationPage.js
+++ frontend/src/pages/RegistrationPage.js
@@ -13,6 +13,7 @@ export default function RegistrationPage() {
             placeholder="Enter your full name"
             id="name"
           />
+          {errors.name && <span className="error-text">{errors.name}</span>}
         </div>
         <div>
           <label htmlFor="email">EMAIL ADDRESS</label>
@@ -22,6 +23,7 @@ export default function RegistrationPage() {
             placeholder="Enter your email"
             id="email"
           />
+          {errors.email && <span className="error-text">{errors.email}</span>}
         </div>
         <div>
           <label htmlFor="password">PASSWORD</label>
@@ -31,6 +33,7 @@ export default function RegistrationPage() {
             placeholder="Enter your password"
             id="password"
           />
+          {errors.password && <span className="error-text">{errors.password}</span>}
         </div>
         <div>
           <label htmlFor="cpassword">CONFIRM PASSWORD</label>
@@ -40,6 +43,7 @@ export default function RegistrationPage() {
             placeholder="Confirm your password"
             id="cpassword"
           />
+           {errors.cpassword && <span className="error-text">{errors.cpassword}</span>}
         </div>
         <div>
           <input type="submit" value="CREATE ACCOUNT" />
