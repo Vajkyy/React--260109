5466c22d508dc47232f7ffb1c852e3be3007de4b
Vajkyy <vajki2014@gmail.com>
2026-01-09 11:14:58 +0100

    Navigation.js update -> welcome+logout button

--- frontend/src/pages/Navigation.js
+++ frontend/src/pages/Navigation.js
@@ -2,7 +2,6 @@ import React from "react";
 import { NavLink } from "react-router";
 
 export default function Navigation() {
-
   return (
     <div>
       <header>
@@ -23,7 +22,12 @@ export default function Navigation() {
               <Navlink to="/mentors">Mentors</Navlink>
             </li>
           </ul>
-
+          <ul>
+            <li>Welcome, {user.user.name ? user.user.name : "Guest"}</li>
+            <li className="bold" onClick={logout}>
+              Logout
+            </li>
+          </ul>
         </nav>
       </header>
     </div>
